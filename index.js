83bea0375bb00034dac1b7169285e20f66c8e3a6
Harshit Kumar <kumarharshit850@gmail.com>
2023-02-04 21:58:13 +0400

    Code cleanup

--- index.js
+++ index.js
@@ -2,68 +2,80 @@ import axios from "axios";
 import moment from "moment";
 import fs from "fs";
 
-async function getListings(params) {
-  // Make the API request with the given address and page size
-  const url = "https://www.vrbo.com/serp/g";
-  const response = await axios.post(url, JSON.stringify(params));
+import { GET_LISTINGS } from "./constants";
 
-  if (!response || !response.data || !response.data.listing) {
+/**
+ * @author Harshit Kumar
+ * @param {*} response
+ * @returns csv file
+ */
+function makeCSVFileOfListings(response) {
+  // Write the header in CSV file
+  const csvContent = `Listing ID,Headline,Start Date,End Date,Price\n`;
+  if (response && response.data && response.data.listings) {
+    // Extract the relevant information from the response
+    const listings = response.data.listings;
+    const today = moment().format("YYYY-MM-DD");
+    const data = [];
+    listings.forEach(listing => {
+      const { listingId, headline, rentNights } = listing;
+      if (rentNights) {
+        // Only include rent rates starting from today
+        rentNights.forEach(rent => {
+          if (moment(rent.startDate).isSameOrAfter(today)) {
+            data.push([
+              listingId,
+              headline,
+              rent.startDate,
+              rent.endDate,
+              rent.price
+            ]);
+          }
+        });
+      }
+    });
+    // Write the extracted data to a CSV file
+    csvContent = data.map(row => row.join(",")).join("\n");
+  } else {
     console.log("The API response is undefined or NULL");
-    // Write the header in CSV file
-    const csvContent = `Listing ID,Headline,Start Date,End Date,Price\n`;
-    fs.writeFileSync("listings.csv", csvContent);
-    console.log(csvContent);
-    return;
   }
 
-  // Extract the relevant information from the response
-  const listings = response.data.listings;
-  const today = moment().format("YYYY-MM-DD");
-  const data = [];
-  listings.forEach(listing => {
-    const { listingId, headline, rentNights } = listing;
-    if (rentNights) {
-      // Only include rent rates starting from today
-      rentNights.forEach(rent => {
-        if (moment(rent.startDate).isSameOrAfter(today)) {
-          data.push([
-            listingId,
-            headline,
-            rent.startDate,
-            rent.endDate,
-            rent.price
-          ]);
-        }
-      });
-    }
-  });
-
-  // Write the extracted data to a CSV file
-  const csvContent =
-    `Listing ID,Headline,Start Date,End Date,Price\n` +
-    data.map(row => row.join(",")).join("\n");
+  // create the CSV file
   fs.writeFileSync("listings.csv", csvContent);
-  console.log(csvContent);
 }
 
+/**
+ * @author Harshit Kumar
+ * @param {*} requestBody
+ */
+async function getListings(requestBody) {
+  // Make the API request with the given address and page size
+  const response = await axios.post(GET_LISTINGS, JSON.stringify(requestBody));
+  makeCSVFileOfListings(response);
+}
+
+/**
+ * @author Harshit Kumar
+ * @param {*} address
+ * @returns true if address is valid
+ */
 function validateAddress(address) {
   const addressRegex = new RegExp(/^[A-Za-z0-9 ]+$/);
   return addressRegex.test(address);
 }
 
 function main() {
-  const params = {
-    q: "500 W Madison St Chicago IL 60607",
+  const requestBody = {
+    q: "500 W Madison St Chicago IL 606079",
     pageSize: 50
   };
 
-  if (!validateAddress(params.q)) {
-    console.log("Address not Valid!");
-    return;
-  } else {
+  if (validateAddress(requestBody.q)) {
     console.log("Address is Valid!, fetching data...");
+    getListings(requestBody);
+  } else {
+    console.log("Address not Valid!");
   }
-  getListings(params);
 }
 
 main();
